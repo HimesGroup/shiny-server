e4226adc4e77100609fb41d37dbeb1b723e79497
Rebecca Greenblatt <rebecca.greenblatt@gmail.com>
2018-08-02 19:01:06 +0000

    got raster download to work for pargasite and fixed ui

--- pargasite/server.R
+++ pargasite/server.R
@@ -7,7 +7,21 @@ library(sp)
 #library(mapview)
 source("labelFormatFunction.R")
 source("month_to_num.R")
+source("getPollutionEstimates.R")
 #library(pargasite)
+
+## load in all bricks
+pm_yearly_brick_full <- brick("../../../../var/www/pargasite_data/pm_yearly_brick_full.grd")
+ozone_yearly_brick_full <- brick("../../../../var/www/pargasite_data/ozone_yearly_brick_full.grd")
+no2_yearly_brick_full <- brick("../../../../var/www/pargasite_data/no2_yearly_brick_full.grd")
+so2_yearly_brick_full <- brick("../../../../var/www/pargasite_data/so2_yearly_brick_full.grd")
+co_yearly_brick_full <- brick("../../../../var/www/pargasite_data/co_yearly_brick_full.grd")
+
+pm_yearly_brick_cropped <- brick("../../../../var/www/pargasite_data/pm_yearly_brick_cropped.grd")
+ozone_yearly_brick_cropped <- brick("../../../../var/www/pargasite_data/ozone_yearly_brick_cropped.grd")
+no2_yearly_brick_cropped <- brick("../../../../var/www/pargasite_data/no2_yearly_brick_cropped.grd")
+so2_yearly_brick_cropped <- brick("../../../../var/www/pargasite_data/so2_yearly_brick_cropped.grd")
+co_yearly_brick_cropped <- brick("../../../../var/www/pargasite_data/co_yearly_brick_cropped.grd")
 
 full_usa = st_as_sf(map("state", plot = FALSE, fill = TRUE))
 
@@ -15,9 +29,24 @@ epa.sites <- read.csv("/srv/shiny-server/pargasite/data/epa_site_locations.csv")
 
 shinyServer(function(input, output, session){
 
-  year.e <- reactive({ pargasite:::bricks.years[[input$year]] })
-  year.c <- reactive({ pargasite:::bricks.years.cropped[[input$year]] })
-
+  poll.e <- reactive({
+    switch(input$pollutant,
+           "PM2.5" = pm_yearly_brick_full,
+           "Ozone" = ozone_yearly_brick_full,
+           "NO2" = no2_yearly_brick_full,
+           "SO2" = so2_yearly_brick_full,
+           "CO" = co_yearly_brick_full)
+  })
+  
+  poll.c <- reactive({
+    switch(input$pollutant,
+           "PM2.5" = pm_yearly_brick_cropped,
+           "Ozone" = ozone_yearly_brick_cropped,
+           "NO2" = no2_yearly_brick_cropped,
+           "SO2" = so2_yearly_brick_cropped,
+           "CO" = co_yearly_brick_cropped)
+  })
+  
   sites <- reactive({ filter(epa.sites, year == as.numeric(input$year) )})
 
   output$notes <- reactive({
@@ -29,25 +58,13 @@ shinyServer(function(input, output, session){
            "CO" = "Mean carbon monoxide (CO) 1-hour average (1971 standard); Units = Parts per million"
     )
   })
-
+  
   ras.e <- reactive({
-    switch(input$pollutant,
-           "PM2.5" = year.e()[[1]],
-           "Ozone" = year.e()[[2]],
-           "NO2" = year.e()[[3]],
-           "SO2" = year.e()[[4]],
-           "CO" = year.e()[[5]]
-    )
+    poll.e()[[(as.numeric(input$year)-2004)]]
   })
-
+  
   ras.c <- reactive({
-    switch(input$pollutant,
-           "PM2.5" = year.c()[[1]],
-           "Ozone" = year.c()[[2]],
-           "NO2" = year.c()[[3]],
-           "SO2" = year.c()[[4]],
-           "CO" = year.c()[[5]]
-    )
+    poll.c()[[(as.numeric(input$year)-2004)]]
   })
 
   trunc.val <- reactive({
@@ -71,7 +88,7 @@ shinyServer(function(input, output, session){
   output$map <- renderLeaflet({
     leaflet(full_usa) %>% addTiles() %>%
       setView(-98.35, 39.5, zoom = 4) %>%
-      addRasterImage(x = ras.t(), colors = palette()) %>%
+      addRasterImage(x = ras.t(), colors = palette(), method = "ngb") %>%
       addPolygons(color = "black", weight = 1, fillColor = "transparent") %>%
       addLegend(pal = colorNumeric(terrain.colors(8), values(ras.t()), na.color = "transparent"),
                 values = values(ras.t()),
@@ -98,14 +115,14 @@ shinyServer(function(input, output, session){
   })
   
   monthyear_start <- reactive({ paste0(month_to_num(input$start_month), "-", input$start_year) })
-  monthyear_end <- reactive({ paste0(month_to_num(input$start_month), "-", input$start_year) })
+  monthyear_end <- reactive({ paste0(month_to_num(input$end_month), "-", input$end_year) })
   
   output$finalDownload <-
     downloadHandler(
       filename <- function() { "pargasite_file.csv" },
       content <- function(file){
         infile <- read.csv(input$user_file$datapath)
-        outfile <- pargasite::getPollutionEstimates.df(infile, monthyear_start(), monthyear_end())
+        outfile <- getPollutionEstimates.df.app(infile, monthyear_start(), monthyear_end())
         write.csv(outfile, file, row.names = FALSE)
       }
     )
