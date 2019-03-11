c297813f10b56b83107c54d2225a4391aec7c3ef
Diwadkar <diwadkar@seas.upenn.edu>
2019-03-11 20:12:07 +0000

    Added DESeq2 results, dropdown gene list and feather format

--- transcriptome_explorer/ui.R
+++ transcriptome_explorer/ui.R
@@ -10,22 +10,23 @@ ui <- shinyUI(fluidPage(
   # Sidebar with a slider input for the number of bins
   sidebarLayout(
     sidebarPanel(
-      p("Expression results for individual gene transcripts using publicly available RNA-Seq datasets."),
+      p("Expression results for individual genes using publicly available RNA-Seq datasets."),
       br(),
       
       selectInput("tissue", label = "Tissue:",
-                  choices = list("Airway Smooth Muscle 1" = "ASM1", "Airway Smooth Muscle 2" = "ASM2", "Airway Epithelium" = "AE"), 
-                  selected="AE"),
+                  choices = list("Airway Smooth Muscle 1" = "ASM1", "Airway Smooth Muscle 2" = "ASM2","Airway Smooth Muscle 3" = "ASM3","Small Airway Epithelial"="SAE"), 
+                  selected="ASM1"),
       
-      textInput(inputId="gene",label="Type the official gene symbol:", value= "GAPDH"),
+      #textInput(inputId="gene",label="Type the official gene symbol:", value= "GAPDH"),
+      uiOutput("genesAvail"), tags$head(tags$style(type="text/css", "#curr_gene {width: 190px}")),
       br(),
       
-      p("Abundance of each transcript is expressed in Transcripts per Million Reads Mapped (TPM) as computed by",
-        a("kallisto.", href="https://pachterlab.github.io/kallisto/", target="_blank"),
-        "Differential expression results were obtained with",
-        a("sleuth.", href="http://pachterlab.github.io/sleuth/", target="_blank"),
-        "The table includes transcripts for which at least 47% of samples had 5 or more reads.",
-        "The plot includes transcripts in the table with average TPM > 1 across all conditions."),
+      p("Read counts of each gene is obtained by HTSeq and normalized by",
+        a("DESeq2.", href="https://www.ncbi.nlm.nih.gov/pubmed/25516281", target="_blank"),
+        "Differential expression results were obtained by DESeq2. Genes with a total read count <10 were filtered out before differential analysis."),
+        # a("sleuth.", href="http://pachterlab.github.io/sleuth/", target="_blank"),
+        # "The table includes transcripts for which at least 47% of samples had 5 or more reads.",
+        # "The plot includes transcripts in the table with average TPM > 1 across all conditions."),
       
       br(),
       #textInput("debugcode", "Debug:", ""),
